891a62e95d8dca8b4806cbe80f61df0c0aae640d
abhrajga <abhishek.rajgaria@publicissapient.com>
2022-09-10 15:46:24 +0530

    using express

---
+++ NodeBasicAsg1/routes/admin.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path.js');
+const router = express.Router();
+
+// /admin
+
+router.get('/add-product',(req, res, next) => {
+    res.sendFile(path.join(rootDir,'../', 'views', 'add-product.html'));
+    
+});
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+module.exports = router;
